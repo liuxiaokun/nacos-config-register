25adc4fc7563bbe2b496fd80a34937ee130d9fac
liuxiaokun <liuxk@cloudoer.com>
2019-04-25 20:08:42 +0800

    refine

---
+++ src/main/java/com/fred/nacos/config/controller/ConfigController.java
@@ -0,0 +1,19 @@
+package com.fred.nacos.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigController {
+
+    @Value("${config.name}")
+    private String name;
+
+    @GetMapping
+    public String getConfig() {
+        return name;
+    }
+}
