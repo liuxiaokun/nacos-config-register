1e6b2209884af01000295ad4892698e6d6bac405
liuxiaokun <liuxk@cloudoer.com>
2019-04-25 21:11:09 +0800

    新增nacos注册服务

--- src/main/java/com/fred/nacos/config/NacosConfigApplication.java
+++ src/main/java/com/fred/nacos/config/NacosConfigApplication.java
@@ -2,8 +2,10 @@ package com.fred.nacos.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NacosConfigApplication {
 
     public static void main(String[] args) {
